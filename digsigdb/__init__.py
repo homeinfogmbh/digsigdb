ecf8b7260103c86560d637810397eca100c9e8c8
Richard Neumann <r.neumann@homeinfo.de>
2019-04-15 10:17:50 +0200

    Refac.

--- digsigdb/__init__.py
+++ digsigdb/__init__.py
@@ -5,7 +5,6 @@ from digsigdb.exceptions import DuplicateUserError
 from digsigdb.orm import CleaningAnnotation
 from digsigdb.orm import CleaningDate
 from digsigdb.orm import CleaningUser
-from digsigdb.orm import Command
 from digsigdb.orm import ProxyHost
 from digsigdb.orm import Screenshot
 from digsigdb.orm import ScreenshotLog
@@ -14,7 +13,6 @@ from digsigdb.orm import Statistics
 
 __all__ = [
     'DuplicateUserError',
-    'Command',
     'Statistics',
     'CleaningUser',
     'CleaningDate',
