c903e68e3b3353a209822ff24fc88e8bbaaef4af
Richard Neumann <r.neumann@homeinfo.de>
2019-04-17 14:29:28 +0200

    Refac.

--- digsigdb/orm.py
+++ digsigdb/orm.py
@@ -4,18 +4,13 @@ Provides ORM models for digital signage
 data that is not part of dscms4.
 """
 from datetime import datetime
-from uuid import uuid4
 
-from peewee import BlobField
 from peewee import BooleanField
 from peewee import CharField
 from peewee import DateTimeField
 from peewee import ForeignKeyField
-from peewee import IntegerField
-from peewee import UUIDField
 
 from mdb import Address, Customer
-from mimeutil import mimetype
 from peeweeplus import MySQLDatabase, JSONModel
 from terminallib import System
 
@@ -29,18 +24,23 @@ __all__ = [
     'CleaningUser',
     'CleaningDate',
     'CleaningAnnotation',
-    'ProxyHost',
-    'Screenshot',
-    'ScreenshotLog']
+    'ProxyHost']
 
 
 DATABASE = MySQLDatabase.from_config(CONFIG['db'])
 
 
+def create_tables():
+    """Creates the tables."""
+
+    for model in MODELS:
+        model.create_table()
+
+
 class _ApplicationModel(JSONModel):
     """Abstract common model."""
 
-    class Meta:     # pylint: disable=C0111
+    class Meta:     # pylint: disable=C0111,R0903
         database = DATABASE
         schema = database.database
 
@@ -78,7 +78,7 @@ class Statistics(_ApplicationModel):
     def to_csv(self, sep=','):
         """Converts the record into a CSV entry."""
         address = self.system.deployment.address
-        timestamp = self.timestamp.isoformat()
+        timestamp = self.timestamp.isoformat()  # pylint: disable=E1101
         fields = (timestamp, str(address), self.document)
         return sep.join(fields)
 
@@ -86,7 +86,7 @@ class Statistics(_ApplicationModel):
 class CleaningUser(_ApplicationModel):
     """Accounts for valet service employees."""
 
-    class Meta:     # pylint: disable=C0111
+    class Meta:     # pylint: disable=C0111,R0903
         table_name = 'cleaning_user'
 
     name = CharField(64)
@@ -132,7 +132,7 @@ class CleaningUser(_ApplicationModel):
 class CleaningDate(_ApplicationModel):
     """Cleaning chart entries."""
 
-    class Meta:     # pylint: disable=C0111
+    class Meta:     # pylint: disable=C0111,R0903
         table_name = 'cleaning_date'
 
     user = ForeignKeyField(CleaningUser, column_name='user')
@@ -200,7 +200,7 @@ class CleaningDate(_ApplicationModel):
 class CleaningAnnotation(_ApplicationModel):
     """Optional annotations for cleaning log entries."""
 
-    class Meta:     # pylint: disable=C0111
+    class Meta:     # pylint: disable=C0111,R0903
         table_name = 'cleaning_annotation'
 
     cleaning_date = ForeignKeyField(
@@ -212,104 +212,11 @@ class CleaningAnnotation(_ApplicationModel):
 class ProxyHost(_ApplicationModel):
     """Valid proxy hosts."""
 
-    class Meta:     # pylint: disable=C0111
+    class Meta:     # pylint: disable=C0111,R0903
         table_name = 'proxy_hosts'
 
     hostname = CharField(255)
 
 
-class Screenshot(_ApplicationModel):
-    """Stores screenshots."""
-
-    uuid = UUIDField(default=uuid4)
-    customer = ForeignKeyField(Customer, column_name='customer')
-    address = ForeignKeyField(Address, column_name='address')
-    _bytes = BlobField(column_name='bytes')
-
-    @classmethod
-    def add(cls, uuid, customer, address, bytes_):
-        """Adds a screenshot."""
-        try:
-            return cls.fetch(uuid, customer, address)
-        except cls.DoesNotExist:
-            screenshot = cls()
-            screenshot.uuid = uuid
-            screenshot.customer = customer
-            screenshot.address = address
-            screenshot.bytes = bytes_
-            screenshot.save()
-            return screenshot
-
-    @classmethod
-    def fetch(cls, uuid, customer, address):
-        """Returns a screenshot by uuid, customer and address."""
-        return cls.get(
-            (cls.uuid == uuid)
-            & (cls.customer == customer)
-            & (cls.address == address))
-
-    @property
-    def bytes(self):
-        """Returns the respective bytes."""
-        return self._bytes
-
-    @bytes.setter
-    def bytes(self, bytes_):
-        """Returns the respective bytes."""
-        self._bytes = bytes_
-
-    @property
-    def mimetype(self):
-        """Returns the data's mime type."""
-        return mimetype(self._bytes)
-
-    @property
-    def log_entries(self):
-        """Yields raw log entry records for this screenshot."""
-        return ScreenshotLog.select().where(
-            (ScreenshotLog.uuid == self.uuid)
-            & (ScreenshotLog.customer == self.customer)
-            & (ScreenshotLog.address == self.address))
-
-    @property
-    def readouts(self):
-        """Yields readouts when this screenshot was shown."""
-        for log_entry in self.log_entries:
-            yield (log_entry.begin, log_entry.end)
-
-
-class ScreenshotLog(_ApplicationModel):
-    """Logs displayed screenshots."""
-
-    class Meta:     # pylint: disable=C0111
-        table_name = 'screenshot_log'
-
-    uuid = UUIDField()
-    customer = ForeignKeyField(Customer, column_name='customer')
-    address = ForeignKeyField(Address, column_name='address')
-    begin = DateTimeField(default=datetime.now)
-    end = DateTimeField(null=True)
-
-    @classmethod
-    def add(cls, uuid, customer, address):
-        """Adds a screenshot log entry."""
-        record = cls()
-        record.uuid = uuid
-        record.customer = customer
-        record.address = address
-        record.save()
-        return record
-
-    @classmethod
-    def close(cls, uuid, customer, address):
-        """Closes the latest screenshot log entry
-        of the provided uuid, customer and address.
-        """
-        record = cls.get(
-            (cls.uuid == uuid)
-            & (cls.customer == customer)
-            & (cls.address == address)
-            & (cls.end >> None)).order_by(cls.begin.desc()).get()
-        record.end = datetime.now()
-        record.save()
-        return record
+MODELS = (
+    Statistics, CleaningUser, CleaningDate, CleaningAnnotation, ProxyHost)
